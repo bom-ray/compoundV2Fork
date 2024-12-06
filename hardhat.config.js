6d84ae4fbc7a8a129c5e3244e6c8f066ff602d8d
bom_ray <bomray67@gmail.com>
2024-12-05 16:31:04 -0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,72 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+           enabled: true,
+           runs: 1000,
+          }
+        }
+      },
+      {
+        version: "0.8.27",
+        settings: {
+          optimizer: {
+           enabled: true,
+           runs: 1000,
+          }
+        }
+      },
+      {
+        version: "0.6.0",
+        settings: {
+          optimizer: {
+           enabled: true,
+           runs: 1000,
+          }
+        }
+      },
+      {
+        version: "0.6.2",
+        settings: {
+          optimizer: {
+           enabled: true,
+           runs: 1000,
+          }
+        }
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+           enabled: true,
+           runs: 1000,
+          }
+        }
+      },
+      {
+        version: "0.5.16",
+        settings: {
+           optimizer: {
+            enabled: true,
+            runs: 1000,
+           }
+         }
+      },
+      {
+        version: "0.8.10",
+        settings: {
+           optimizer: {
+            enabled: false,
+            runs: 1000,
+           }
+         }
+      }
+    ]
+  },
+};
